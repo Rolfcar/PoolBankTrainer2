868d62e4aabde7edbccb8721cbddf931369f0e73
Rolf Carlson <rolf_carlson@msn.com>
2026-01-01 18:36:18 -0500

    Initial Commit

---
+++ PoolBankTrainer/PoolBankTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoolBankTrainerApp.swift
+//  PoolBankTrainer
+//
+//  Created by Rolf Carlson on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct PoolBankTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
